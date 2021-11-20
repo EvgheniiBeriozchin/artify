7b587a27187a42c16ebd1579771cbde5dc93af67
unknown <schimbatorlaura@yahoo.com>
2021-11-20 13:39:45 +0100

    add sqlalchemy

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,29 @@
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+from datetime import datetime
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:''@localhost/artify'
+app.config['SQLALCHEMY_TRACK_NOTIFICATIONS'] = False
 
-@app.route('/', methods=['GET'])
+db = SQLAlchemy(app)
+
+
+class Articles(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text())
+    body = db.Column(db.Text())
+    date = db.Column(db.DateTime, default=datetime.now())
+
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+
+
+@app.route('/get', methods=['GET'])
 def get_articles():
     return jsonify({"Hello": "World"})
 
